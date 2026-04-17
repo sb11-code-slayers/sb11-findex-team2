faa34b9c390e4a7d6a6415ebf2ea0e5cef88e6ea
kim gwanhee <rhksgml55@gmail.com>
2026-04-17 18:33:07 +0900

    feat: KrxOpenApiClient에 날짜 기준 지수 정보 목록 조회 메서드 추가

--- src/main/java/com/sprint/mission/findex/domain/syncclient/client/KrxOpenApiClientImpl.java
+++ src/main/java/com/sprint/mission/findex/domain/syncclient/client/KrxOpenApiClientImpl.java
@@ -6,6 +6,7 @@ import com.sprint.mission.findex.domain.syncclient.dto.KrxApiResponseWrapper;
 import com.sprint.mission.findex.global.exception.ApiException;
 import com.sprint.mission.findex.global.exception.ApiException.ERROR;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -17,7 +18,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.util.UriUtils;
 
 @Component
 public class KrxOpenApiClientImpl implements KrxOpenApiClient {
@@ -55,21 +56,22 @@ public class KrxOpenApiClientImpl implements KrxOpenApiClient {
         }
 
         try {
+            String encodedIndexName = UriUtils.encode(indexName, StandardCharsets.UTF_8);
             int pageNo = 1;
             int numOfRows = 1000;
             List<IndexDataApiResponse> allItems = new ArrayList<>();
 
             while (true) {
-                URI uri = UriComponentsBuilder.fromHttpUrl(baseUrl + "/getStockMarketIndex")
-                        .queryParam("serviceKey", apiKey)
-                        .queryParam("resultType", "json")
-                        .queryParam("pageNo", pageNo)
-                        .queryParam("numOfRows", numOfRows)
-                        .queryParam("beginBasDt", from.format(DateTimeFormatter.BASIC_ISO_DATE))
-                        .queryParam("endBasDt", to.format(DateTimeFormatter.BASIC_ISO_DATE))
-                        .queryParam("idxNm", indexName)
-                        .build(true)
-                        .toUri();
+                String url = baseUrl + "/getStockMarketIndex"
+                        + "?serviceKey=" + apiKey
+                        + "&resultType=json"
+                        + "&pageNo=" + pageNo
+                        + "&numOfRows=" + numOfRows
+                        + "&beginBasDt=" + from.format(DateTimeFormatter.BASIC_ISO_DATE)
+                        + "&endBasDt=" + to.format(DateTimeFormatter.BASIC_ISO_DATE)
+                        + "&idxNm=" + encodedIndexName;
+
+                URI uri = URI.create(url);
                 String responseBody = restTemplate.getForObject(uri, String.class);
 
                 if (responseBody == null || responseBody.isBlank()) {
@@ -127,15 +129,15 @@ public class KrxOpenApiClientImpl implements KrxOpenApiClient {
             List<IndexDataApiResponse> allItems = new ArrayList<>();
 
             while (true) {
-                URI uri = UriComponentsBuilder.fromHttpUrl(baseUrl + "/getStockMarketIndex")
-                        .queryParam("serviceKey", apiKey)
-                        .queryParam("resultType", "json")
-                        .queryParam("pageNo", pageNo)
-                        .queryParam("numOfRows", numOfRows)
-                        .queryParam("beginBasDt", date.format(DateTimeFormatter.BASIC_ISO_DATE))
-                        .queryParam("endBasDt", date.plusDays(1).format(DateTimeFormatter.BASIC_ISO_DATE))
-                        .build(true)
-                        .toUri();
+                String url = baseUrl + "/getStockMarketIndex"
+                        + "?serviceKey=" + apiKey
+                        + "&resultType=json"
+                        + "&pageNo=" + pageNo
+                        + "&numOfRows=" + numOfRows
+                        + "&beginBasDt=" + date.format(DateTimeFormatter.BASIC_ISO_DATE)
+                        + "&endBasDt=" + date.format(DateTimeFormatter.BASIC_ISO_DATE);
+
+                URI uri = URI.create(url);
                 String responseBody = restTemplate.getForObject(uri, String.class);
 
                 if (responseBody == null || responseBody.isBlank()) {
