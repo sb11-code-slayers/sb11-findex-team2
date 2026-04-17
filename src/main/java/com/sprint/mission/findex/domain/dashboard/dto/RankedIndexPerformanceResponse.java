cce5ae3cfd56fad161d8e53da90cf5dea9c4a5d1
shyunii <evkm37@gmail.com>
2026-04-18 04:08:05 +0900

    feat: Ranking API 구현

---
+++ src/main/java/com/sprint/mission/findex/domain/dashboard/dto/RankedIndexPerformanceResponse.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.findex.domain.dashboard.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "순위가 포함된 지수 성과 정보 응답")
+public record RankedIndexPerformanceResponse(
+    @Schema(description = "지수 성과 정보")
+    IndexPerformanceResponse perfomance,
+
+    @Schema(description = "순위", example = "1")
+    Integer rank
+) {
+}
